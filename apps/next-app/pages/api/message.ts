b1aff721196b482ac15c92ab4adfc95b0c5cc3a8
Malloc72P <scra1028@gmail.com>
2023-03-27 01:35:58 +0900

    activeMQ 사용해서 성공함

--- apps/next-app/pages/api/message.ts
+++ apps/next-app/pages/api/message.ts
@@ -1,37 +1,44 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import amqplib from 'amqplib';
+import mqtt from 'mqtt';
+import { nanoid } from 'nanoid';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { ServerConfigs } from '../../server/config';
 import { MessageDTO } from '../../shared/dto/MessageDTO';
+
+const { awsMqttEndpoint, queueName, mqUsername, mqPw } = ServerConfigs.envs;
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<MessageDTO>
 ) {
+  const publishDate = new Date();
+  const message = JSON.stringify({
+    message: nanoid(20),
+    publishDate,
+  });
+
   try {
-    const { awsMqEndpoint, queueName } = ServerConfigs.envs;
-    const message = 'ping';
-
-    const connection = await amqplib.connect(awsMqEndpoint);
-    const channel = await connection.createChannel();
-    await channel.assertQueue(queueName);
-    await channel.sendToQueue(queueName, Buffer.from(message));
-
-    console.debug('API >>> message >>> publish message : ', message);
-
-    await channel.close();
-    await connection.close();
+    const client = mqtt.connect(awsMqttEndpoint, {
+      protocol: 'wss',
+      username: mqUsername,
+      password: mqPw,
+      clientId: nanoid(6),
+    });
+    client.on('connect', () => {
+      console.log('do publish');
+      client.publish(queueName, message);
+    });
 
     res.status(200).json({
-      message: 'ping',
-      publishDate: new Date(),
+      message,
+      publishDate,
     });
   } catch (error) {
     console.error(error);
 
     res.status(500).json({
       message: '몬가...몬가 잘못되었습니다...!',
-      publishDate: new Date(),
+      publishDate,
     });
   }
 }
--- apps/next-app/pages/index.tsx
+++ apps/next-app/pages/index.tsx
@@ -1,33 +1,50 @@
 import { Box, Flex } from '@mantine/core';
-import mqtt from 'mqtt';
-import { ReactElement, useEffect, useState } from 'react';
+import { Client } from 'mqtt';
+import { ReactElement, useCallback, useEffect, useState } from 'react';
 import DefaultLayout from '../components/DefaultLayout';
 import { DivRotator } from '../components/DivRotator';
 import { ServerConfigs } from '../server/config';
 import { NextPageWithLayout } from './_app';
-
-const subscribe = async () => {
-  const { awsMqttEndpoint, mqUsername, mqPw, queueName } = ServerConfigs.envs;
-  const connection = mqtt.connect(awsMqttEndpoint, {
-    protocol: 'mqtt',
-    username: mqUsername,
-    password: mqPw,
-    clientId: crypto.randomUUID(),
-  });
-
-  connection.subscribe(queueName, (error) => {
-    console.error(error);
-  });
-  connection.on(queueName, (topic: string, message: unknown) => {
-    console.debug(`subscription >>> topic(${topic}) : `, message);
-  });
-};
+import mqtt from 'mqtt';
+import { nanoid } from 'nanoid';
 
 const Home: NextPageWithLayout = () => {
   const [deg, setDeg] = useState(0);
+  const [client, setClient] = useState<Client | null>(null);
+  const { awsMqttEndpoint, mqUsername, mqPw, queueName } = ServerConfigs.envs;
+
+  const subscribe = useCallback(async () => {
+    const client = mqtt.connect(awsMqttEndpoint, {
+      protocol: 'wss',
+      username: mqUsername,
+      password: mqPw,
+      clientId: nanoid(6),
+    });
+
+    client.on('connect', () => {
+      client.subscribe(queueName, (error) => {
+        if (error) {
+          console.error(error);
+        }
+        console.log('start subscribing');
+      });
+
+      client.on('message', (topic: string, message: Buffer) => {
+        console.log(`[${topic}] message: `, message.toString());
+      });
+
+      setClient(client);
+    });
+  }, [awsMqttEndpoint, mqPw, mqUsername, queueName]);
 
   useEffect(() => {
     subscribe();
+
+    return () => {
+      if (client) {
+        client.unsubscribe(queueName);
+      }
+    };
   }, []);
 
   return (
