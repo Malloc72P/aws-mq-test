a867a78fee72ee8735391ce51873f59ebee71790
Malloc72P <scra1028@gmail.com>
2023-03-29 23:35:54 +0900

    불필요한 구성 제거

--- apps/next-app/shared/config.ts
+++ apps/next-app/shared/config.ts
@@ -1,6 +1,5 @@
 export const ServerConfigs = {
   envs: {
-    queueName: process.env.NEXT_PUBLIC_QUEUE_NAME ?? '',
     awsMqEndpoint: process.env.NEXT_PUBLIC_MQ_ENDPOINT ?? '',
     mqUsername: process.env.NEXT_PUBLIC_MQ_USERNAME ?? '',
     mqPw: process.env.NEXT_PUBLIC_MQ_PW ?? '',
